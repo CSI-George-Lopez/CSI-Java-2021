8904e998e3751775ab695ca617dea6592901ccc0
GEORGE-HP-SPECT\George Lopez <25-025@sanignacio.pr>
2021-08-20 09:07:36 -0400

    Wrote Hello World.

---
+++ Modules/Module2/src/csi/lopez/pkg/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package csi.lopez.pkg;
+
+/**
+ * @author George Lopez
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		System.out.println("George Lopez");
+		System.out.println("25-025");
+		
+
+	}
+
+}
