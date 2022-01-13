dded22fa160d823e04b209dbf444cca4e8fc15ae
GEORGE-HP-SPECT\George Lopez <25-025@sanignacio.pr>
2022-01-13 09:31:00 -0400

    m

--- MainProject/src/csi/lopez/snake/Board.java
+++ MainProject/src/csi/lopez/snake/Board.java
@@ -50,7 +50,7 @@ public class Board extends JPanel implements ActionListener {
     private void initBoard() {
 
         addKeyListener(new TAdapter());
-        setBackground(Color.black);
+        setBackground(new Color(50, 150, 150));
         setFocusable(true);
 
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
